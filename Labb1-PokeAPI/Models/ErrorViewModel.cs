e6c40bc4dbb0aa3fde94361da339a63536208e84
VISORKILLEN <viktor.gustavsson@live.se>
2026-08-24 19:37:24 +0200

    Add project files.

---
+++ Labb1-PokeAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Labb1_PokeAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Labb1-PokeAPI/Models/PokemonListRepsonse.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Labb1_PokeAPI.Models
+{
+    public class PokemonListRepsonse
+    {
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<PokemonResult> Results { get; set; } = new();
+    }
+
+    public class PokemonResult
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+    }
+}
