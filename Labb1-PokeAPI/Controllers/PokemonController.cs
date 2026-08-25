54610233be326e0d6e934548c0cc8e899290d874
VISORKILLEN <viktor.gustavsson@live.se>
2026-08-25 09:27:31 +0200

    Added contructor that takes IpokemonService//Viktor

--- Labb1-PokeAPI/Controllers/PokemonController.cs
+++ Labb1-PokeAPI/Controllers/PokemonController.cs
@@ -6,8 +6,17 @@ namespace Labb1_PokeAPI.Controllers
 {
     public class PokemonController : Controller
     {
+        private readonly IPokemonService _pokemonService;
+
+        // Constructor that takes an IPokemonService as a dependency
+        public PokemonController(IPokemonService pokemonService)
+        {
+            _pokemonService = pokemonService;
+        }
+
         public IActionResult Index()
         {
+
             return View();
         }
     }
