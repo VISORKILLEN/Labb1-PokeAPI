158138542d2f90c37673992ee2cf2eca8b9bda50
VISORKILLEN <viktor.gustavsson@live.se>
2026-08-25 11:21:44 +0200

    added a middleware to handle statuscode 404//Viktor

---
+++ Labb1-PokeAPI/Middlewares/ErrorMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace Labb1_PokeAPI.Middlewares
+{
+    public class ErrorMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        // This method is called for each HTTP request and allows the middleware to process the request and response.
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"I ErrorMiddleware:{context.Request.Path}");
+            await _next(context);
+
+            Console.WriteLine($"I ErrorMiddleware:{context.Response.StatusCode}");
+
+            if (context.Response.StatusCode == 404)
+            {
+                context.Items["Message"] = "Detta är inte rätt sida lilla vän";
+                context.Request.Path = "/Home/Error";
+
+                await _next(context);
+            }
+        }
+    }
+}
--- Labb1-PokeAPI/Program.cs
+++ Labb1-PokeAPI/Program.cs
@@ -1,4 +1,5 @@
 using Labb1_PokeAPI.Services;
+using Labb1_PokeAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ErrorMiddleware>();
 app.UseRouting();
 
 app.UseAuthorization();
